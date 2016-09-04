9408e8befe4865f0fb75318c740c3438e335252e
Tobias Butler <tobias@change.org>
2016-09-04 13:11:38 -0700

    add and remove from list

--- app/client/routes/edit.js
+++ app/client/routes/edit.js
@@ -7,19 +7,62 @@ module.exports = function edit(app) {
       const listOpts = JSON.parse(listContainer.dataset.opts);
       const listEl = listContainer.querySelector('ul');
       const appendToListButton = listContainer.querySelector('.js-append-to-list');
-      let listLength = _.get(editorOpts.editorSource, listOpts.listPath).length;
+      let list = _.get(editorOpts.editorSource, listOpts.listPath);
+
+      function updateList() {
+        list = _.map(listEl.children, listItem => {
+          return _.reduce(listItem.querySelectorAll('input, textarea'), (memo, input) => {
+            const inputOpts = JSON.parse(input.dataset.opts);
+            _.set(memo, inputOpts.path, input.value);
+            return memo;
+          }, {});
+        });
+        _.set(editorOpts.editorSource, listOpts.listPath, list);
+      }
+
+      function addItemListener(listItemEl, itemIndex) {
+        const deleteButton = listItemEl.querySelector('.js-delete-item');
+        deleteButton.addEventListener('click', e => {
+          e.preventDefault();
+          updateList();
+          list.splice(itemIndex, 1);
+          const newList = document.createElement('ul');
+          _.forEach(list, (listItem, itemElIndex) => {
+            const newItem = document.createElement('li');
+            newItem.innerHTML = app.handlebars.partials.listitem({
+              listItem,
+              itemIndex: itemElIndex,
+              fields: listOpts.field.itemFields,
+              editorConfig: editorOpts.editorConfig,
+              editorSource: editorOpts.editorSource,
+              fieldPrefix: `${listOpts.listPath}.${itemElIndex}.`,
+            });
+            newList.appendChild(newItem);
+          });
+          listEl.innerHTML = newList.innerHTML;
+          addItemListeners();
+        });
+      }
+
+      function addItemListeners() {
+        _.forEach(listEl.children, addItemListener);
+      }
+
+      addItemListeners();
 
       appendToListButton.addEventListener('click', e => {
         e.preventDefault();
         const newItem = document.createElement('li');
-        newItem.innerHTML = app.handlebars.partials.fields({
+        newItem.innerHTML = app.handlebars.partials.listitem({
+          itemIndex: list.length,
           fields: listOpts.field.itemFields,
           editorConfig: editorOpts.editorConfig,
           editorSource: editorOpts.editorSource,
-          fieldPrefix: `${listOpts.listPath}.${listLength}.`,
+          fieldPrefix: `${listOpts.listPath}.${list.length}.`,
         });
+        addItemListener(newItem, list.length);
+        list.push({});
         listEl.appendChild(newItem);
-        listLength++;
         return false;
       });
     });
--- app/lib/traverse_and_extend.js
+++ app/lib/traverse_and_extend.js
@@ -2,7 +2,7 @@ const traverse = require('traverse');
 
 module.exports = (base, newData) => {
   traverse(newData).forEach(function setLeafs(datum) {
-    if (this.isLeaf) _.set(base, this.path, datum);
+    if (this.isLeaf || _.isArray(datum)) _.set(base, this.path, datum);
   });
   return base;
 };
